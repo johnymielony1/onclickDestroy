7a80c38dfab7604d7bfd4acc8aa9bf9c7c919706
johnymielony1 <janek.potoczek@gmail.com>
2021-01-06 15:46:26 +0100

    poprawione (nie działa)

--- js/script.js
+++ js/script.js
@@ -1,27 +1,12 @@
 'use strict'; 
+var btns = [];
 
-const btn1 = document.getElementById('btn1');
-const btn2 = document.getElementById('btn2');
-const btn3 = document.getElementById('btn3');
-const btn4 = document.getElementById('btn4');
-const btn5 = document.getElementById('btn5');
+btns = document.querySelectorAll('button');
 
-btn1.addEventListener('click', () => {
-    btn1.style.display = 'none';
-});
+console.log(btns);
 
-btn2.addEventListener('click', () => {
-    btn2.style.display = 'none';
-});
-
-btn3.addEventListener('click', () => {
-    btn3.style.display = 'none';
-});
-
-btn4.addEventListener('click', () => {
-    btn4.style.display = 'none';
-});
-
-btn5.addEventListener('click', () => {
-    btn5.style.display = 'none';
-});
+btns.forEach(element => {
+    addEventListener('click', () => { 
+        btns[element].style.display = 'none';
+    })
+})
