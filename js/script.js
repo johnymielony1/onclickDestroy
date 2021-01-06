e5fd6321965c08ccb0253f738bcf34ab94f46d81
johnymielony1 <janek.potoczek@gmail.com>
2021-01-06 01:01:04 +0100

    initial commit

---
+++ js/script.js
@@ -0,0 +1,27 @@
+'use strict'; 
+
+const btn1 = document.getElementById('btn1');
+const btn2 = document.getElementById('btn2');
+const btn3 = document.getElementById('btn3');
+const btn4 = document.getElementById('btn4');
+const btn5 = document.getElementById('btn5');
+
+btn1.addEventListener('click', () => {
+    btn1.style.display = 'none';
+});
+
+btn2.addEventListener('click', () => {
+    btn2.style.display = 'none';
+});
+
+btn3.addEventListener('click', () => {
+    btn3.style.display = 'none';
+});
+
+btn4.addEventListener('click', () => {
+    btn4.style.display = 'none';
+});
+
+btn5.addEventListener('click', () => {
+    btn5.style.display = 'none';
+});
